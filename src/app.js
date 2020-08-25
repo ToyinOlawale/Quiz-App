a6241fbde16b8219a40aab0affef34aa13f30016
Olawale Toyin <tokedavids4@gmail.com>
2020-08-25 16:18:28 +0100

    First stage of completion

--- src/app.js
+++ src/app.js
@@ -1,55 +1,182 @@
 const questionNumber = document.querySelector('.question-number');
 const questionText = document.querySelector ('.question-text');
 const optionContainer = document.querySelector ('.option-container');
+const answerIndicatorContainer = document.querySelector('.answers-indicator')
+const homeBox = document.querySelector('.home-box');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
 
 let questionCounter= 0; 
 let currentQuestion;
 let availableQuestions = [];
 let availableOptions = [];
+let correctAnswers = 0;
+let attempts= 0;
 
 
 function setAvailableQuestions (){
  const totalQuestion = quiz.length;
- quiz.forEach(quiz => availableQuestions.push(quiz))
-}
-
-function getNewQuestion(){
- //  set question number
- questionNumber.innerHTML = `Question ${questionCounter + 1} of ${quiz.length}`
-  // set random question
-  const index = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]; 
-  currentQuestion = index;
-  questionText.innerHTML = currentQuestion.question;
-  //get position index of current question from questions array
-  const index1 = availableQuestions.indexOf(index);
-  //  remove the question index from the question array so the question does not repeat
- availableQuestions.splice(index1,1);
-
-  //  set options
- // get length of options 
- const optionLength = currentQuestion.options.length;
-  const currentOptions = currentQuestion.options
-  currentOptions.forEach(option => availableOptions.push(option));  
-
- // create options in HTML
-  availableOptions.forEach(option => {  
-    const options = `<div class="option">${option}</div>
-    <div class="option">${option}</div>
-    <div class="option">${option}</div>
-    <div class="option">${option}</div>` 
-    optionContainer.innerHTML= options    
-  });  
-  questionCounter ++   
+ for(let i=0;i<totalQuestion; i++) {
+   availableQuestions.push(quiz[i])
+ }
 };
 
-function next(){
- questionCounter  === quiz.length ? console.log('Over'): getNewQuestion(); 
+function getNewQuestion(){
+ //set question number
+ questionNumber.innerHTML = `Question ${questionCounter + 1} of ${quiz.length}`
+ //set random question
+ const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]; 
+ currentQuestion = questionIndex ;
+ questionText.innerHTML = currentQuestion .question;
+ //get position index of current question from questions array
+ const index1 = availableQuestions.indexOf(questionIndex);
+ //  remove the question index from the question array so the question does not repeat
+ availableQuestions.splice(index1,1);
+
+ //set options
+ // get length of options  
+   const optionLength = currentQuestion.options.length;
+   for(let i=0; i< optionLength; i++) {
+      availableOptions.push(i)
+    }
+    optionContainer.innerHTML=''; //to correct the doubling options mistake
+    let animationDelay = 0.15;
+     //create random options in HTML 
+    for(let i=0; i< optionLength; i++) {
+      const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length )];
+      const index2 = availableOptions.indexOf (optionIndex);         
+      availableOptions.splice(index2,1); 
+      const option = document.createElement('div');
+      option.innerHTML= currentQuestion.options[optionIndex];
+      option.id = optionIndex;
+      // next 2 lines for animation delay of options
+      option.style.animationDelay = animationDelay + 's';
+      animationDelay = animationDelay + 0.15 ;
+      option.className= "option";
+      optionContainer.appendChild(option) 
+      option.setAttribute("onclick", "getResult(this)")      
+    };
+ questionCounter++   
+};
+
+//to get result of current attempt question
+function getResult (optionElement) {
+  const id = parseInt(optionElement.id); //to convert string from div element to number 
+  if (id === currentQuestion.answer){
+    // set green color to correct option
+    optionElement.classList.add ('correct');
+    // add indicaor to correct option 
+    updateAnswerIndicator('correct');    
+    correctAnswers++;
+  } else {
+    optionElement.classList.add ('wrong');
+     // add indicaor to wrong option 
+    updateAnswerIndicator('wrong');   
+    // if answer is wrong after clicking then show the correct answer by adding green color
+    const optionLength = optionContainer.children.length;
+    for(let i=0; i< optionLength; i++) {
+       if (parseInt(optionContainer.children[i].id) === currentQuestion.answer){
+        optionContainer.children[i].classList.add('correct')
+       }
+    }
+
+  };
+  attempts++;
+
+  unclickableOptions ();  
+};
+
+// to make all the options unclickable once the user selects an option
+function unclickableOptions(){
+ const optionLength = optionContainer.children.length;
+ for (let i=0; i<optionLength; i++){
+   optionContainer.children[i].classList.add("already-answered");
+ }
+};
+
+function answerIndicator(){
+  answerIndicatorContainer.innerHTML='';
+  const totalQuestion = quiz.length;
+  for(let i= 0; i< totalQuestion; i++){
+    const indicator = document.createElement('div');
+    answerIndicatorContainer.appendChild(indicator);  
+  }
+};
+
+function updateAnswerIndicator(markType) {
+  answerIndicatorContainer.children[questionCounter-1].classList.add(markType) 
+};
+
+
+function next(){  
+  questionCounter == quiz.length   ? quizOver(): getNewQuestion(); 
+};
+
+function quizOver(){
+  //hide quix box
+  quizBox.classList.add('hide');
+  //show result box
+  resultBox.classList.remove('hide');
+  quizResult();
 }
 
-window.onload = function (){
+function quizResult(){
+  resultBox.querySelector('.total-question').innerHTML = quiz.length;
+  resultBox.querySelector('.total-attempt').innerHTML = attempts;
+  resultBox.querySelector('.total-correct').innerHTML = correctAnswers;
+  resultBox.querySelector('.total-wrong').innerHTML = attempts - correctAnswers;
+  const percentage = (correctAnswers/quiz.length)*100;
+  resultBox.querySelector('.percentage').innerHTML = percentage.toFixed(0) + '%';
+  resultBox.querySelector('.total-score').innerHTML = correctAnswers + '/' + quiz.length;
+  const message = document.getElementById('message');
+  if (percentage <= 40){
+    message.innerHTML= 'Nice try..Please you need to get familiar!'
+  } else if (percentage > 40 && percentage <= 70) {
+    message.innerHTML= "Kudos! You know me to a large extent❤"
+  } else {
+    message.innerHTML= 'You definately belong in my inner circle. Cheers to greatness🥂'
+  };
+}
+
+function resetQuiz(){
+  questionCounter=0;
+  correctAnswers=0;
+  attempts=0;
+}
+
+function tryAgainQuiz(){
+   //hide the resultbox
+   resultBox.classList.add('hide');
+   //show quiz box
+   quizBox.classList.remove('hide');
+   resetQuiz();  
+   startQuiz();
+}
+
+
+ function backHome(){
+  //hide the resultbox
+  resultBox.classList.add('hide');
+  //show quiz box
+  homeBox.classList.remove('hide');
+ }
+
+function startQuiz(){
+
+  //hide home box
+   homeBox.classList.add('hide');
+   //show quiz box
+   quizBox.classList.remove('hide');
+   
 //  set all questions in an array
   setAvailableQuestions();
   // set question number
   getNewQuestion();
   next();
-}
+  answerIndicator();
+ }
+
+
+ window.onload = function(){
+   homeBox.querySelector('.total-question').innerHTML = quiz.length; 
+ }
--- src/questions.js
+++ src/questions.js
@@ -2,28 +2,78 @@
 
 const quiz = [
   {
-   question :'Which month comes right after June?',
-   options: ['may','september','july','august'],
-   answer: 0
+   question :'What is my middle name?',
+   options: ['Emmanuela','Esther','Joy','Elizabeth'],
+   answer: 1
   },
   {
-    question :'Which month comes right after august?',
-    options: ['may','september','july','august'],
-    answer: 1
-   },
-   {
-    question :'Which month comes right after april?',
-    options: ['may','september','july','august'],
+  question :'What is my favourite color?',
+    options: ['Purple','Red','Black','Blue'],
     answer: 2
-   },
-   {
-    question :'Which month comes right after november?',
-    options: ['may','December','july','hello'],
+  },
+  {
+   question :'What did I study in the University?',
+    options: ['Medicine','Theatre Arts','Chemical Engineering','Electrical Electronic Engineering'],
     answer: 3
-   },
+  },
+  {
+    question :'Which of these states do I hail from?',
+    options: ['Ondo State','Osun State','Lagos State','Oyo State'],
+    answer: 0
+  },
+  {
+    question :'What is my favourite Hobby?',
+    options: ['Watching Movies','Singing','Twitter','Dancing'],
+    answer: 2
+  },
+  {
+    question :'What is my favourite Nickname?',
+    options: ['Toyin Tomatoe','Blackie','Lepa-tohbad','All of the above'],
+    answer: 3
+  },
+  {
+    question :'What do you think I consider my best physical asset?',
+    options: ['Lips','Body','Face','Legs'],
+    answer: 1
+  },
+  {
+    question :'What do you think I may be insecure about sometimes ?',
+    options: ['Forhead','Skin Color','Face','Nothing'],
+    answer: 2
+  },
+  {
+   question :'Who is my favourite person on earth?',
+   options: ['My mother','My brother','My Bestfriend','My Boyfriend'],
+   answer: 1
+  },
+  {
+   question :'Which of these words best describes me?',
+   options: ['Pessimist','Optimist','Realist','Idealist'],
+   answer: 2
+  },
+  {
+   question :'What would I rather drink?',
+   options: ['Water','Soda','Yogurt','Juice'],
+   answer: 2
+  },
+  {
+   question :'What would I preferrably wear to a date?',
+   options: ['Jeans and Shirt','Gown','Jumpsuit','Skirt and Top'],
+   answer: 1
+  },
+  {
+   question :"Which of these do you think I'm likely to get first?",
+   options: ['Piercings','Tatoos','Surgical Implants','Braces'],
+   answer: 3
+  },
+  {
+   question :'What country have I always wanted to visit?',
+   options: ['Germany','Canada','England','United States of America'],
+   answer: 0
+  },
    {
-    question :'Which month comes right after october?',
-    options: ['may','november','july','hi'],
-    answer: 4
-   }
-]
+   question :'What is my worst pet peeve ?',
+   options: ['Aggressive Interrupters','Slow Internet','Grammar Correctors','Constant cell phone distraction'],
+   answer: 1
+  }
+];
